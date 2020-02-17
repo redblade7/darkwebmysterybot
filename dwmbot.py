7b827a2f6fa6ccbc676860c820c6037126fb03a7
redblade7 <15269155+redblade7@users.noreply.github.com>
2020-02-16 20:20:29 -0500

    update version

--- dwmbot.py
+++ dwmbot.py
@@ -33,7 +33,7 @@ from mastodon import Mastodon,MastodonError
 import argparse,json,secrets,sys
 
 #program version
-progver = "4-dev (20200125)"
+progver = "4-dev (20200217)"
 
 #prefix
 prefix = ["dark net","dark web","deep web","marianas net","marianas web","shadow net","shadow web"]
